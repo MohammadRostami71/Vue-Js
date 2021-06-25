ba662f27c36b0842a0a990d560625a3152f904c9
Mohammad Rostami <mohammadeostami13@gmail.com>
2021-06-26 00:17:26 +0430

    7. Moving to a Better Development Setup & Workflow with the Vue CLI

---
+++ 07-moving-to-a-better-development-setup-workflow-with-the-vue-cli/src/components/FriendContact.vue
@@ -0,0 +1,37 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "FriendContact",
+  data(){
+    return{
+      detailsAreVisible: false,
+      friend:
+          {
+            id: 'Manuel ',
+            name: 'Manuel Lorenz',
+            phone: '01234 5678 991',
+            email: 'manuel@localhost.com'
+          }
+    };
+  },
+  methods:{
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
