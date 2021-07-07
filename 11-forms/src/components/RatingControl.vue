5bf4ce8b83b7899ba7433e1f2c03210a757d9925
Mohammad Rostami <mohammadeostami13@gmail.com>
2021-07-08 01:25:15 +0430

    Forms

---
+++ 11-forms/src/components/RatingControl.vue
@@ -0,0 +1,69 @@
+<template>
+  <ul>
+    <li :class="{active:modelValue === 'Poor'}">
+      <button @click="activate('Poor')" type="button">Poor</button>
+    </li>
+    <li :class="{active:modelValue === 'Average'}">
+      <button @click="activate('Average')" type="button">Average</button>
+    </li>
+    <li :class="{active:modelValue === 'Great'}">
+      <button @click="activate('Great')" type="button">Great</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "RatingControl",
+  props:['modelValue'],
+  emits:['update:modelValue'],
+  // data() {
+  //   return {
+  //     activeOption: this.modelValue
+  //   };
+  // },
+  computed:{
+    activeOption() {
+      return this.modelValue;
+    }
+  },
+  methods: {
+    activate(option) {
+      this.$emit('update:modelValue',option)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.active {
+  border:3px solid  #3a0061;
+}
+
+.active button {
+  border:3px solid  #3a0061;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0.5rem 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  font: inherit;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
